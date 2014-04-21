8a94a955229cac77f7e15ef1193e2891b572b898
fox <linpinger@gmail.com>
2014-04-21 11:15:24 +0800

    2014-04-21 : add isEnd field to bookinfo activity

--- src/com/linpinger/foxbook/Activity_BookInfo.java
+++ src/com/linpinger/foxbook/Activity_BookInfo.java
@@ -15,7 +15,7 @@ import android.widget.TextView;
 public class Activity_BookInfo extends Activity {
 	private Button btn_save;
 	private TextView tv_bid;
-	private EditText edt_bname, edt_qdid, edt_burl, edt_delurl;
+	private EditText edt_bname, edt_isend, edt_qdid, edt_burl, edt_delurl;
 	
 	private int bookid ;
 	
@@ -23,6 +23,7 @@ public class Activity_BookInfo extends Activity {
 		btn_save = (Button) findViewById(R.id.btn_save);
 		tv_bid = (TextView) findViewById(R.id.tv_bid);
 		edt_bname = (EditText) findViewById(R.id.edt_bname);
+		edt_isend = (EditText) findViewById(R.id.edt_isend);
 		edt_qdid = (EditText) findViewById(R.id.edt_qdid);
 		edt_burl = (EditText) findViewById(R.id.edt_burl);
 		edt_delurl = (EditText) findViewById(R.id.edt_delurl);
@@ -38,9 +39,10 @@ public class Activity_BookInfo extends Activity {
 		
 		init_controls() ; // 初始化各控件
 		// 显示数据
-		Map<String,String> info = FoxDB.getOneRow("select name as bn, url as bu, qidianid as qid, delurl as list from book where id=" + bookid);
+		Map<String,String> info = FoxDB.getOneRow("select name as bn, url as bu, isEnd as bend, qidianid as qid, delurl as list from book where id=" + bookid);
 		tv_bid.setText(String.valueOf(bookid)) ;
 		edt_bname.setText(info.get("bn"));
+		edt_isend.setText(info.get("bend"));
 		edt_qdid.setText(info.get("qid"));
 		edt_burl.setText(info.get("bu"));
 		edt_delurl.setText(info.get("list"));
@@ -53,6 +55,7 @@ public class Activity_BookInfo extends Activity {
 				cc.put("URL", edt_burl.getText().toString());
 				cc.put("DelURL", edt_delurl.getText().toString());
 				cc.put("QiDianID", edt_qdid.getText().toString());
+				cc.put("isEnd", edt_isend.getText().toString());
 				FoxDB.update_cell("Book", cc, "id=" + bookid) ; // 修改单个字段
 				setResult(RESULT_OK, (new Intent()).setAction("已修改书籍信息"));
 				finish();
