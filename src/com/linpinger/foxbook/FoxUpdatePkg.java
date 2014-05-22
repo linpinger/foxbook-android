618d20f9314c450c60820722c8dc684bf4e37237
fox <linpinger@gmail.com>
2014-05-22 10:32:32 +0800

    2014-05-22 : add calc sha1 sum when update to avoid cdn cash

--- src/com/linpinger/foxbook/FoxUpdatePkg.java
+++ src/com/linpinger/foxbook/FoxUpdatePkg.java
@@ -1,12 +1,17 @@
 package com.linpinger.foxbook;
 
+import android.content.Context;
 import java.util.HashMap;
 
-import android.content.Context;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+import java.math.BigInteger;
 
 
 public class FoxUpdatePkg {
 	private Context mContext;
+	private String apkPATH = "/sdcard/FoxBook.apk" ;
 	private String urlVersion = "http://linpinger.qiniudn.com/version";
 	private String urlAPK = "http://linpinger.qiniudn.com/FoxBook.apk";
 	
@@ -18,13 +23,16 @@ public class FoxUpdatePkg {
 		HashMap<String, Object> remoteVer = getRemoteVersion();
 		int newVer = Integer.valueOf((String)remoteVer.get("date")); // 远程日期
 		String newURL = (String)remoteVer.get("url");
+		String newSHA1 = (String)remoteVer.get("sha1");
 		int oldVer = getVersion(mContext); // 本程序日期
 		if ( newVer <= oldVer ) { return 0 ; }
 		if ( newURL == "" ) {
-			FoxBookLib.saveHTTPFile(urlAPK, "/sdcard/FoxBook.apk");
+			FoxBookLib.saveHTTPFile(urlAPK, apkPATH);
 		} else {
-			FoxBookLib.saveHTTPFile(newURL, "/sdcard/FoxBook.apk") ;
+			FoxBookLib.saveHTTPFile(newURL, apkPATH) ;
 		}
+		String realSHA1 = getFileMD5(new File(apkPATH), "SHA1") ;
+		if ( newSHA1.compareToIgnoreCase(realSHA1) != 0 ) { return 0 ; } // sha1值不对
 		return newVer;
 	}
 
@@ -50,5 +58,37 @@ public class FoxUpdatePkg {
 		}
 	    return Integer.valueOf(versionCode);
 	}
+
+	    /**
+     * 
+     * @param file 
+     * @param algorithm 所请求算法的名称  for example: MD5, SHA1, SHA-256, SHA-384, SHA-512 etc. 
+     * @return
+     */
+    public static String getFileMD5(File file,String algorithm) {
+        if (!file.isFile()) {
+            return null;
+        }
+
+        MessageDigest digest = null;
+        FileInputStream in = null;
+        byte buffer[] = new byte[1024];
+        int len;
+
+        try {
+            digest = MessageDigest.getInstance(algorithm);
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+        } catch (Exception e) {
+//            e.printStackTrace();
+            return "";
+        }
+
+        BigInteger bigInt = new BigInteger(1, digest.digest());
+        return bigInt.toString(16);
+    }
 	
 }
